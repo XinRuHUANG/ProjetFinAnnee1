968695d461cecbb214a362b8143f3cb85c7ec8bf
Anthusan <anthusan.s@gmail.com>
2025-05-24 15:50:57 +0200

    Add files via upload

--- AlgoSousGradient3.0.py
+++ AlgoSousGradient3.0.py
@@ -39,7 +39,7 @@ def Generateur_Graphes(n=6, width=100, height=100, seed=None, show=True):
 
     return MatriceDistance, coords
 
-def resoudre_probleme_primal(D, lambdas):
+def resoudre_relaxation (D, lambdas):
     """
     Résout le problème primal relaxé en minimisant le lagrangien L(x, lambda)
     """
@@ -48,8 +48,8 @@ def resoudre_probleme_primal(D, lambdas):
     meilleure_solution = None
     meilleur_cycle = None
 
-    for permutation in itertools.permutations(range(n)):
-        cycle = list(permutation) + [permutation[0]]
+    for ordre_villes in itertools.permutations(range(n)):
+        cycle = list(ordre_villes) + [ordre_villes[0]]
         matrice_x = np.zeros((n, n))
         cout_cycle = 0
         for i in range(n):
@@ -66,9 +66,9 @@ def resoudre_probleme_primal(D, lambdas):
 
     return meilleure_solution, meilleur_cout, meilleur_cycle
 
-def methode_du_sous_gradient(D, nb_iterations=10, alpha_initial=1.0):
+def sous_gradient(D, nb_iterations=10, alpha_initial=1.0):
     """
-    Applique la méthode du sous-gradient sur le dual du TSP relaxé.
+    Applique la méthode du sous-gradient sur le dual du PVC relaxé.
     """
     n = len(D)
     lambdas = np.zeros(n)
@@ -76,7 +76,7 @@ def methode_du_sous_gradient(D, nb_iterations=10, alpha_initial=1.0):
     cycles_optimaux = []
 
     for k in range(nb_iterations):
-        matrice_x, cout_dual, cycle = resoudre_probleme_primal(D, lambdas)
+        matrice_x, cout_dual, cycle = resoudre_relaxation(D, lambdas)
         gradient = np.array([1 - sum(matrice_x[i]) for i in range(n)])
         alpha_k = alpha_initial / (k + 1)
         lambdas = np.maximum(0, lambdas + alpha_k * gradient)
@@ -85,7 +85,7 @@ def methode_du_sous_gradient(D, nb_iterations=10, alpha_initial=1.0):
 
     return lambdas, valeurs_duales, cycles_optimaux
 
-def afficher_graphe_avec_cycle(D, coords, cycle_optimal):
+def visualiser_cycle_optimal (D, coords, cycle_optimal):
     """
     Affiche le graphe complet avec les arêtes pondérées et surligne le cycle optimal en rouge.
     """
@@ -95,32 +95,31 @@ def afficher_graphe_avec_cycle(D, coords, cycle_optimal):
     for i in range(n):
         for j in range(i + 1, n):
             G.add_edge(i, j, weight=round(D[i][j], 1))
-
     pos = {i: (coords[i][0], coords[i][1]) for i in range(n)}
 
-    edge_colors = []
-    edges = G.edges()
-    optimal_edges = {(cycle_optimal[i], cycle_optimal[i+1]) for i in range(len(cycle_optimal)-1)}
-    optimal_edges |= {(v, u) for u, v in optimal_edges}
+    couleurs_aretes = []
+    liste_aretes = G.edges()
+    aretes_optimales = {(cycle_optimal[i], cycle_optimal[i+1]) for i in range(len(cycle_optimal)-1)}
+    aretes_optimales |= {(v, u) for u, v in aretes_optimales}
 
-    for u, v in edges:
-        if (u, v) in optimal_edges or (v, u) in optimal_edges:
-            edge_colors.append('red')
+    for u, v in liste_aretes:
+        if (u, v) in aretes_optimales or (v, u) in aretes_optimales:
+            couleurs_aretes.append('red')
         else:
-            edge_colors.append('gray')
+            couleurs_aretes.append('gray')
 
     plt.figure(figsize=(6, 6))
-    nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color=edge_colors, node_size=500, font_size=10)
-    edge_labels = nx.get_edge_attributes(G, 'weight')
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=8)
+    nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color=couleurs_aretes, node_size=500, font_size=10)
+    poids_aretes = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=poids_aretes, font_size=8)
     plt.title("Graphe des villes avec le chemin optimal en rouge")
     plt.axis('equal')
     plt.show()
 
 # Exécution
-matriceDistance, coords = Generateur_Graphes(n=5, seed=42, show=False)
-lambdas_finaux, valeurs_duales, cycles_final = methode_du_sous_gradient(matriceDistance)
-afficher_graphe_avec_cycle(matriceDistance, coords, cycles_final[-1])
+matriceDistance, coords = Generateur_Graphes(n=6, seed=42, show=False)
+lambdas_finaux, valeurs_duales, cycles_final = sous_gradient(matriceDistance)
+visualiser_cycle_optimal(matriceDistance, coords, cycles_final[-1])
 print("Matrice des distances :\n", matriceDistance)
 print("Multiplicateurs finaux λ :", lambdas_finaux)
 print("Valeurs duales :", valeurs_duales)
